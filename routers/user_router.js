dcb199dd9d1473f6ecc2d2bf33e76d4733f15e5f
souhaib <s.belmaksene@gmail.com>
2024-05-14 19:35:49 +0200

    let s gooooooo

--- routers/user_router.js
+++ routers/user_router.js
@@ -2,16 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const UserController = require('../controller/user.controller');
-const UserService = require("../services/user.services"); // Import UserService module
+const UserService = require("../services/user.services");
 
-// Define the route for handling POST requests to "/login"
+
+
 router.post('/registration', UserController.register);
 router.post('/login', UserController.login);
-
-// POST /api/save-location
 router.post('/saveLocation',UserController.saveLocation);
-
-// Define the route for handling POST requests to "/registration"
 router.post('/otpLogin', UserController.otpLogin);
 router.post('/verifyOTP', UserController.verifyOTP);
 
