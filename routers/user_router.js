1f2391bfd6613ecc42926c5516e16b013b7fe971
souhaib <s.belmaksene@gmail.com>
2024-05-15 10:17:26 +0200

    add geolocationtower and gettowerinfo

--- routers/user_router.js
+++ routers/user_router.js
@@ -10,8 +10,9 @@ router.post('/registration', UserController.register);
 router.post('/login', UserController.login);
 router.post('/saveLocation',UserController.saveLocation);
 
-router.get('/towers', towerController.getTowerLocations);
-router.get('/tower/:id', towerController.getTowerInfo);
+router.get('/towers', UserService.getTowerLocations); 
+router.get('/tower/:id', UserService.getTowerInfo); 
+
 
 router.post('/otpLogin', UserController.otpLogin);
 router.post('/verifyOTP', UserController.verifyOTP);
